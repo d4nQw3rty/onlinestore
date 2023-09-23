21bead6b2abf202e7aab4a13db61be585781691c
Danys Linares <danlinmoy@gmail.com>
2023-09-23 09:21:00 -0400

    Fix linters

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,12 +1,11 @@
 require 'test_helper'
-class ProductController_test < ActionDispatch::IntegrationTest
-    test 'render a list of products' do
-        get products_path
-        assert_response :succes
-        assert_select '.product'
-    end
+class ProductControllerTest < ActionDispatch::IntegrationTest
+  test 'render a list of products' do
+    get products_path
+    assert_response :succes
+    assert_select '.product'
+  end
 
-    test 'render a detailed product page' do
-
-    end
+  test 'render a detailed product page' do
+  end
 end
