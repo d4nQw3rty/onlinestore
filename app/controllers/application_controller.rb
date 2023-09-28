1877d7dee09f329bed51582205427af33f23139a
Danys Linares <danlinmoy@gmail.com>
2023-09-28 18:31:38 -0400

    get language from header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    I18n.with_locale(locale_from_header, &action)
+  end
+
+  private
+
+  def locale_from_header
+    lang = request.env['HTTP_ACCEPT_LANGUAGE']
+    lang.present? ? lang.scan(/^[a-z]{2}/).first : 'es'
+  end
 end
