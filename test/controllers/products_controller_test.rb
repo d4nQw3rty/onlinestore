80931a55021d5919072566f1e2dcac22ba81fc04
Danys Linares <danlinmoy@gmail.com>
2023-10-05 10:46:02 -0400

    fix text

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,20 +3,20 @@ class ProductControllerTest < ActionDispatch::IntegrationTest
   test 'render a list of products' do
     get products_path
     assert_response :success
-    assert_select '.product', 3
-    assert_select '.category', 3
+    assert_select '.product', 12
+    assert_select '.category', 9
   end
 
   test 'render a list of products filtered by category' do
     get products_path(category_id: categories(:computers).id)
     assert_response :success
-    assert_select '.product', 1
+    assert_select '.product', 5
   end
 
   test 'render a list of products filtered by min price and max price' do
     get products_path(min_price: 160, max_price: 200)
     assert_response :success
-    assert_select '.product', 1
+    assert_select '.product', 3
     assert_select 'h2', 'Nintendo Switch'
   end
 
@@ -30,28 +30,28 @@ class ProductControllerTest < ActionDispatch::IntegrationTest
   test 'order products by expensive price first' do
     get products_path(order_by: 'expensive')
     assert_response :success
-    assert_select '.product', 3
-    assert_select '.products .product:first-child h2', 'Macbook Air'
+    assert_select '.product', 12
+    assert_select '.products .product:first-child h2', 'Seat Panda clásico'
   end
 
   test 'order products by cheapest price first' do
     get products_path(order_by: 'cheapest')
     assert_response :success
-    assert_select '.product', 3
-    assert_select '.products .product:first-child h2', 'PS4 fat'
+    assert_select '.product', 12
+    assert_select '.products .product:first-child h2', 'El hobbit'
   end
 
   test 'order products by newest first' do
     get products_path(order_by: 'newest')
     assert_response :success
-    assert_select '.product', 3
-    assert_select '.products .product:first-child h2', 'PS4 fat'
+    assert_select '.product', 12
+    assert_select '.products .product:first-child h2', 'Macbook Air'
   end
 
   test 'render a detailed product page' do
     get product_path(products(:ps4))
     assert_response :success
-    assert_select '.title', 'PS4 fat'
+    assert_select '.title', 'PS4 Fat'
     assert_select '.description', 'PS4 en buen estado'
     assert_select '.price', '150$'
   end
