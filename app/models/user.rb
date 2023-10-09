c146c288c7b725ae05c8656afae4eace30fe6c34
Danys Linares <danlinmoy@gmail.com>
2023-10-09 12:41:40 -0400

    model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validate :email, presence: true, uniqueness: true
+  validate :username, presence: true, uniqueness: true
+  validate :password, length: { minimun: 6 }
 end
