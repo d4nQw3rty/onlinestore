c7e0748600e8bfe286f1bc8173fc43a0513e351a
Danys Linares <danlinmoy@gmail.com>
2023-10-09 13:15:18 -0400

    shorter routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :categories, except: :show
   resources :products, path: '/'
 
-  namespace :authentication do
+  namespace :authentication, path: '', as: '' do
     resources :users, only: %i[new create]
   end
 end
