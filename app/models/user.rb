92500d5f34a0ca776620125e9ce0bc471a05f55e
Danys Linares <danlinmoy@gmail.com>
2023-10-10 10:59:24 -0400

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+  format: {
+    with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+    message: :invalid
+  }
   validates :username, presence: true, uniqueness: true,
     length: {in: 3..15},
     format: { 
@@ -9,4 +13,13 @@ class User < ApplicationRecord
     message: :invalid
     }
   validates :password, length: { minimum: 6 }
+
+  before_save :downcase_attributes
+
+  private
+
+  def downcase_attributes
+    self.username.downcase
+    self.email.downcase
+  end
 end
