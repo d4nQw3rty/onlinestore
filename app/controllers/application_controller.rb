ed0d320f4bb9838f586bc9b3c16b12aa2112ce6d
Danys Linares <danlinmoy@gmail.com>
2023-10-20 09:31:03 -0400

    add authorize pilicies to products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
 
+  class NotAuthorizeError < StandardError; end
+
+  rescue_from NotAuthorizeError do
+    redirect_to products_path, alert: t('common.not_authorized') unless is_allowed
+  end
+
   around_action :switch_locale
   before_action :set_current_user
   before_action :protect_pages
@@ -21,5 +27,10 @@ class ApplicationController < ActionController::Base
 
   def protect_pages
     redirect_to new_session_path, alert: t('common.not_logged_in') unless Current.user
+  end
+
+  def authorize! product
+    is_allowed = product.user_id == Current.user.id
+    raise NotAuthorizeError unless is_allowed
   end
 end
